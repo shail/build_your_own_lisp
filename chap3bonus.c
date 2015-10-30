ba89c1b2e3c4a54566981662db7be336bfedc0de
Shail Patel <shailpatel2@gmail.com>
2015-10-30 13:28:44 -0700

    Finish chapter 3 basics

--- chap3bonus.c
+++ chap3bonus.c
@@ -2,25 +2,28 @@
 
 void print_hello_world();
 void while_hello_world();
+void do_while_loop();
 
 void print_hello_world_n_times(int n)
 {
     for (int i = 0; i < n; i++) {
-        printf("Hello, world\n");
+        printf("0Hello, world\n");
     }
 }
 
 int main(int argc, char *argv[])
 {
-   print_hello_world(); 
+   print_hello_world();
    while_hello_world();
    print_hello_world_n_times(10);
+   do_while_loop();
+   return 0;
 }
 
 void print_hello_world()
 {
     for (int i = 0; i < 5; i++) {
-        printf("Hello, world\n");
+        printf("1Hello, world\n");
     }
 }
 
@@ -28,7 +31,18 @@ void while_hello_world()
 {
     int i = 5;
     while (i > 0) {
-        printf("Hello, world\n");
+        printf("2Hello, world\n");
         i--;
     }
 }
+
+void do_while_loop()
+{
+    int i = 0;
+    do
+    {
+        printf("3Hello, world\n");
+        i++;
+    } while (i < 5);
+}
+
